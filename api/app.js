1c328d2bcb3f69203728077a4bf99f087709a5c2
Gaurav Yuvraj <gyuvraaj123@gmail.com>
2025-06-14 00:58:40 +0530

    fixed code

--- api/app.js
+++ api/app.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 app.use(express.json());
 
 
-const userModel = require('./model/user')
+const userModel = require('../model/user')
 
 app.get('/',(req,res)=>{
 res.send('hello')

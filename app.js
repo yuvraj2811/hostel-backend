2f7cb0cd1daadb77b17c94703de4cacc3cd7da80
Gaurav Yuvraj <gyuvraaj123@gmail.com>
2025-06-14 04:30:36 +0530

    add all

--- app.js
+++ app.js
@@ -15,12 +15,18 @@ next()
 })
 
 app.post('/create', async (req,res)=>{
-    let {name,email,password}= req.body;
+    let {id,name,email,password,hostel, duration,institution,course,}= req.body;
 
     const creatUser = await userModel.create({
+        id,
         name,
         email,
-        password
+        password,
+        hostel,
+        duration,
+        institution,
+        course
+
     })
 
     res.send(creatUser)
@@ -31,6 +37,36 @@ app.get('/getall' ,async (req,res)=>{
     const alluser =await userModel.find()
     res.send(alluser);
 })
+
+app.get('/get/:id' ,async (req,res)=>{
+
+    const user =await userModel.findOne({id:parseInt(req.params.id)})
+    res.send(user);
+})
+
+app.put('/update/:id', async (req, res) => {
+  const updatedUser = await userModel.findOneAndUpdate(
+    { id: parseInt(req.params.id) },  // Find user by id
+    {
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+      hostel: req.body.hostel,
+      duration: req.body.duration,
+      institution: req.body.institution,
+      course: req.body.course,
+    },
+    { new: true }
+  );
+  res.send(updatedUser);
+});
+
+
+app.delete('/delete/:id', async (req, res) => {
+  const deletedUser = await userModel.findOneAndDelete({ id: parseInt(req.params.id) });
+  res.send(deletedUser);
+});
+
 
 
 
--- model/user.js
+++ model/user.js
@@ -6,7 +6,12 @@ mongoose.connect(process.env.MONGODB);
 const userSchema = mongoose.Schema({
   name:String,
   email:String,
-  password:String
+  password:String,
+  hostel:String,
+  duration:String,
+  institution:String,
+  course:String,
+  id:Number
 })
 
 module.exports = mongoose.model("user",userSchema)

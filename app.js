16a451a10bfd3b927ae360f9c9e27b7337ac069f
Gaurav Yuvraj <gyuvraaj123@gmail.com>
2025-06-14 17:30:44 +0530

    add

--- app.js
+++ app.js
@@ -92,4 +92,7 @@ app.post('/createcom', async (req,res)=>{
   })
 
 
-app.listen(process.env.PORT)
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});

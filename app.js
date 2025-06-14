03d67dde4bf48faaf33abc3e7159a2ba047452bf
Gaurav Yuvraj <gyuvraaj123@gmail.com>
2025-06-14 14:37:29 +0530

    add files

--- app.js
+++ app.js
@@ -3,14 +3,15 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
+const cors = require('cors');
 app.use(express.json());
-
+app.use(cors());
 
 const userModel = require('./model/user')
 
 app.get('/',(req,res,next)=>{
 res.send('hello')
-next()
+
 
 })
 
